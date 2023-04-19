83ceb126ab9e9a1f4820776369aa413ea2274063
Uros Dasic <uros1992dasic@gmail.com>
2023-04-19 11:43:31 +0200

    add wrapper

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -1,11 +1,27 @@
-import React from "react";
 
-const Wrapper = () => {
+import Menu from "./Menu";
+import Nav from "./Nav";
+
+type WrapperProps = {
+    children: React.ReactNode;
+};
+
+const Wrapper = (props: WrapperProps) => {
+
     return (
         <>
-            Wrapper
+            <Nav />
+
+            <div className="container-fluid">
+                <div className="row">
+                    <Menu />
+                    <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+                        {props.children}
+                    </main>
+                </div>
+            </div>
         </>
-    )
+    );
 }
 
 export default Wrapper;

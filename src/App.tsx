8bb8d651d8b31b3fb28300b1bef551dcef86ba2d
Uros Dasic <uros1992dasic@gmail.com>
2023-04-19 06:53:33 +0200

    add pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,20 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
+import Dashboard from './pages/Dashboard';
+import Profile from './pages/Profile';
+import Register from './pages/Register';
+import Login from './pages/Login';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path={"/"} />
+          <Route path={"/"} element={<Dashboard />} />
+          <Route path={"/profile"} element={<Profile />} />
+          <Route path={"/register"} element={<Register />} />
+          <Route path={"/login"} element={<Login />} />
         </Routes>
 
       </BrowserRouter>

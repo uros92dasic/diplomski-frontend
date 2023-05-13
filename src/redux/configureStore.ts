6e0af37e03c12f8acd9a97c00b2c07a840d630eb
Uros Dasic <uros1992dasic@gmail.com>
2023-05-13 12:11:59 +0200

    fix permissions page redirection on reload

--- src/redux/configureStore.ts
+++ src/redux/configureStore.ts
@@ -2,11 +2,30 @@ import { configureStore } from "@reduxjs/toolkit";
 import { setUserReducer } from "./reducers/setUserReducer";
 import messageSlice from "./slices/messageSlice";
 import { searchReducer } from "./reducers/setProductSearchReducer";
+import storage from 'redux-persist/lib/storage';
+import { persistStore, persistReducer } from 'redux-persist';
+import { combineReducers } from 'redux';
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['user'] // Only persist the 'user' state
+};
+
+const rootReducer = combineReducers({
+    user: setUserReducer,
+    message: messageSlice,
+    search: searchReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-    reducer: {
-        user: setUserReducer,
-        message: messageSlice,
-        search: searchReducer,
-    },
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: false,
+        }),
 });
+
+export const persistor = persistStore(store);
